d1fa834ea918d095296a33774b227114a3745032
moytho <moytho@hotmail.com>
2016-04-14 20:01:37 -0600

    First commit

---
+++ DragAndDrop/Scripts/app.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+
+// Declare app level module which depends on other modules
+var app =   angular.module('app', [
+    'as.sortable',
+    'ui.bootstrap'
+]);
+
+app.controller('dragDropController', ['$scope', '$window',
+    function ($scope, $window) {
+
+        $scope.employees = [];
+        $scope.data = 
+            { 
+                employees: [
+                { ID: 1, FirstName: "Erick" },
+                { ID: 2, FirstName: "Moises" },
+                { ID: 3, FirstName: "Jonathan" }
+            ],
+                projects: [
+                { ID: 1, FirstName: "Test" },
+                ]
+            };
+        
+        $scope.setDate = function(project){
+            console.log(project);
+        };
+
+
+
+        $scope.sortableOptions = {
+            itemMoved: function (event) {
+                console.log(event);
+                console.log(event.dest);
+                console.log(event.source);
+            },
+            orderChanged: function (event) {
+                console.log(event);
+            },
+            containment: '#row'
+        };
+
+    }]);
